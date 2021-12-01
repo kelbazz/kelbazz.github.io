bc87cdc586910957d1dc6773e7378d576c30ed66
Kelbaz <81097895+Kodbaz@users.noreply.github.com>
2021-12-01 18:58:52 +0100

    ok

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,18 @@
-addEventListener("load", () => {
-  let tileContainer = document.querySelector("#tile-container");
-  let repeatTime = getComputedStyle(document.documentElement).getPropertyValue(
-    "--repeat-time"
-  );
+let tileContainer = document.querySelector("#tile-container");
+let repeatTime = getComputedStyle(document.documentElement).getPropertyValue(
+  "--repeat-time"
+);
+let tiles = document.querySelectorAll(".tile");
 
-  for (let i = 0; i < Math.pow(repeatTime, 2); i++) {
-    let tile = document.createElement("div");
-    tile.classList.add("tile");
-    tile.onmousedown = () => {
-        tile.classList.toggle("stay");
-    }
-    tileContainer.appendChild(tile);
-  }
+for (let i = 0; i < Math.pow(repeatTime, 2); i++) {
+  let tile = document.createElement("div");
+  tile.classList.add("tile");
+  tile.onmousedown = () => {
+    tile.classList.toggle("stay");
+  };
+  tileContainer.appendChild(tile);
+}
+
+setInterval(() => {
+  tiles[Math.floor(Math.random() * tiles.length)].click();
 });
