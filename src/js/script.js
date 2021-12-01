f867fef6bbf12315d17c5b7758f11bfec61f9fd3
Kelbaz <81097895+Kodbaz@users.noreply.github.com>
2021-12-01 18:11:56 +0100

    ok

--- src/js/script.js
+++ src/js/script.js
@@ -4,7 +4,7 @@ addEventListener("load", () => {
     "--tile-size"
   );
 
-  for (let i = 0; i < Math.pow(tileSize, 2); i++) {
+  for (let i = 0; i < Math.pow(parseInt(tileSize), 2); i++) {
     let tile = document.createElement("div");
     tile.classList.add("tile");
     tileContainer.appendChild(tile);
