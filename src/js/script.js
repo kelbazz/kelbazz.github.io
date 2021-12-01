81be8e21ed2512b2a995c27048b5614f68ad2465
Kelbaz <81097895+Kodbaz@users.noreply.github.com>
2021-12-01 19:02:41 +0100

    ok

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,6 @@ let tileContainer = document.querySelector("#tile-container");
 let repeatTime = getComputedStyle(document.documentElement).getPropertyValue(
   "--repeat-time"
 );
-let tiles = document.querySelectorAll(".tile");
 
 for (let i = 0; i < Math.pow(repeatTime, 2); i++) {
   let tile = document.createElement("div");
@@ -12,6 +11,8 @@ for (let i = 0; i < Math.pow(repeatTime, 2); i++) {
   };
   tileContainer.appendChild(tile);
 }
+
+let tiles = document.querySelectorAll(".tile");
 
 setInterval(() => {
   tiles[Math.floor(Math.random() * tiles.length)].click();
