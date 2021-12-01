d0e1877be9ac997fb62f5e95bce9325f77b9f5cf
Kelbaz <81097895+Kodbaz@users.noreply.github.com>
2021-12-01 18:09:27 +0100

    ok

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,12 @@
-let tileContainer = document.querySelector("#tile-container");
-let tileSize = getComputedStyle(document.documentElement).getPropertyValue("--tile-size");
+addEventListener("load", () => {
+  let tileContainer = document.querySelector("#tile-container");
+  let tileSize = getComputedStyle(document.documentElement).getPropertyValue(
+    "--tile-size"
+  );
 
-for (let i = 0; i < Math.pow(tileSize, 2); i++) {
+  for (let i = 0; i < Math.pow(tileSize, 2); i++) {
     let tile = document.createElement("div");
     tile.classList.add("tile");
     tileContainer.appendChild(tile);
-}
+  }
+});
