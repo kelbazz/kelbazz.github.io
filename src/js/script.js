31f40419378a497506da064b2190ce7ebf0bb014
Kelbaz <81097895+Kodbaz@users.noreply.github.com>
2021-12-08 13:05:45 +0100

    ok

--- src/js/script.js
+++ src/js/script.js
@@ -1,38 +1,65 @@
-document.documentElement.style.setProperty(
-  "--repeat-x",
-  getDivider(window.innerWidth, 10)
-);
-document.documentElement.style.setProperty(
-  "--repeat-y",
-  getDivider(window.innerHeight, 10)
-);
+document.addEventListener("DOMContentLoaded", function () {
+  var link = document.createElement("link");
+  link.rel = "stylesheet";
+  link.href = "https://fonts.googleapis.com/css?family=Lexend+Deca";
+  document.head.appendChild(link);
 
-let tileContainer = document.querySelector("#tile-container");
-let repeatX = getComputedStyle(document.documentElement).getPropertyValue(
-  "--repeat-x"
-);
-let repeatY = getComputedStyle(document.documentElement).getPropertyValue(
-  "--repeat-y"
-);
+  var h1 = document.createElement("h1");
+  h1.innerHTML = "Kelbaz";
+  document.body.appendChild(h1);
 
-for (let i = 0; i < repeatX * repeatY; i++) {
-  let tile = document.createElement("div");
-  tile.classList.add("tile");
-  tile.addEventListener("click", () => {
-    tile.classList.toggle("stay");
+  document.body.style.fontFamily = "Lexend Deca";
+
+  h1.classList.add("title");
+
+  h1.style.margin = "auto";
+  h1.style.display = "block";
+
+  h1.style.position = "absolute";
+  h1.style.top = "50%";
+  h1.style.left = "50%";
+
+  h1.style.transform = "translate(-50%, -50%)";
+
+  h1.style.fontSize = "3em";
+
+  document.body.style.backgroundColor = "#0099ff";
+
+  h1.style.color = "#ffffff";
+
+  h1.style.transformOrigin = "center center";
+
+  var button = document.createElement("button");
+  button.innerHTML = "Click me";
+  document.body.appendChild(button);
+
+  button.style.position = "absolute";
+  button.style.top = "50%";
+  button.style.left = "50%";
+
+  button.style.transform = "translate(-50%, 50px)";
+
+  button.style.border = "1px solid #ffffff";
+  button.style.backgroundColor = "#0099ff";
+  button.style.color = "#ffffff";
+  button.style.padding = "10px";
+  button.style.fontSize = "1em";
+  button.style.borderRadius = "5px";
+
+  button.addEventListener("mouseover", function () {
+    button.style.backgroundColor = "#ffffff";
+    button.style.color = "#0099ff";
   });
-  tileContainer.appendChild(tile);
-}
+  button.addEventListener("mouseout", function () {
+    button.style.backgroundColor = "#0099ff";
+    button.style.color = "#ffffff";
+  });
 
-let tiles = tileContainer.querySelectorAll(".tile");
+  button.style.transition = "all 0.2s";
 
-setInterval(() => {
-  tiles[Math.floor(Math.random() * tiles.length)].click();
-}, 500);
-
-function gcd(a, b) {
-  if (b === 0) {
-    return a;
-  }
-  return gcd(b, a % b);
-}
+  button.addEventListener("click", function () {
+    var colors = ["#0099ff", "#9900FF", "#FF0033", "#FFFF00", "#00FF33"];
+    document.body.style.backgroundColor =
+      colors[Math.floor(Math.random() * colors.length)];
+  });
+});
