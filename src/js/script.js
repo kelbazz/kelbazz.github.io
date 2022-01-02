dd61211364e71e9d1d0d79a10a79ae7436f8d6a2
Kelbaz <81097895+Kodbaz@users.noreply.github.com>
2022-01-02 16:01:17 +0100

    ok

--- src/js/script.js
+++ src/js/script.js
@@ -1,67 +1,8 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var link = document.createElement("link");
-  link.rel = "stylesheet";
-  link.href = "https://fonts.googleapis.com/css?family=Lexend+Deca";
-  document.head.appendChild(link);
+let canvas = document.querySelector("#canvas");
+let ctx = canvas.getContext("2d");
 
-  var h1 = document.createElement("h1");
-  h1.innerHTML = "Kelbaz";
-  document.body.appendChild(h1);
+canvas.width = 600;
+canvas.height = 150;
 
-  document.body.style.fontFamily = "Lexend Deca";
+ctx.beginPath()
 
-  h1.classList.add("title");
-
-  h1.style.margin = "auto";
-  h1.style.display = "block";
-
-  h1.style.position = "absolute";
-  h1.style.top = "50%";
-  h1.style.left = "50%";
-
-  h1.style.transform = "translate(-50%, -50%)";
-
-  h1.style.fontSize = "3em";
-
-  document.body.style.backgroundColor = "#0099ff";
-
-  h1.style.color = "#ffffff";
-
-  h1.style.transformOrigin = "center center";
-
-  var button = document.createElement("button");
-  button.innerHTML = "Click me";
-  document.body.appendChild(button);
-
-  button.style.position = "absolute";
-  button.style.top = "50%";
-  button.style.left = "50%";
-
-  button.style.transform = "translate(-50%, 50px)";
-
-  button.style.border = "1px solid #ffffff";
-  button.style.backgroundColor = "#0099ff";
-  button.style.color = "#ffffff";
-  button.style.padding = "10px";
-  button.style.fontSize = "1em";
-  button.style.borderRadius = "5px";
-
-  button.addEventListener("mouseover", function () {
-    button.style.backgroundColor = "#ffffff";
-    button.style.color = "#0099ff";
-  });
-  button.addEventListener("mouseout", function () {
-    button.style.backgroundColor = "#0099ff";
-    button.style.color = "#ffffff";
-  });
-
-  button.style.transition = "all 0.2s";
-
-  button.addEventListener("click", function () {
-    var colors = ["#0099ff", "#9900FF", "#FF0033", "#FFFF00", "#00FF33"];
-    document.body.style.backgroundColor =
-      colors[Math.floor(Math.random() * colors.length)];
-  });
-});
-
-document.body.style.transition = 'background-color 0.2s';
