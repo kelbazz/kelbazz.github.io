7aaac75c114f0998bc5fbe025dfea7f00e497d9c
Kelbaz <81097895+Kodbaz@users.noreply.github.com>
2022-03-02 17:03:06 +0100

    ok

--- src/js/os.js
+++ src/js/os.js
@@ -20,6 +20,15 @@ let kos = {
       /*/ Render Graphic User Interface /*/
 
       osDiv.innerHTML = `
+              <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+                <defs>
+                  <filter id="goo-filter">
+                    <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
+                    <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7" result="goo" />
+                    <feBlend in="SourceGraphic" in2="goo" />
+                  </filter>
+                </defs>
+              </svg>
               <div class='desktop'>Desktop</div>
               `;
 
