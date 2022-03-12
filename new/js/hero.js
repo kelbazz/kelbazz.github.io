7a5010d98d948b082d38b7a55badacf685092f88
Kelbaz <81097895+Kodbaz@users.noreply.github.com>
2022-03-13 00:17:20 +0100

    ok

--- new/js/hero.js
+++ new/js/hero.js
@@ -29,7 +29,8 @@ class Logo {
 
   draw() {
     this.img.addEventListener("load", () => {
-      ctx.drawImage(this.img, this.x, this.y, this.size.x, this.size.y);
+      // ctx.drawImage(this.img, this.x, this.y, this.size.x, this.size.y);
+      ctx.fillRect(this.x, this.y, this.size.x, this.size.y);
     });
   }
 }
