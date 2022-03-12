9d654a37f49315a4a382b60845a924de1bb67c05
Kelbaz <81097895+Kodbaz@users.noreply.github.com>
2022-03-12 23:27:46 +0100

    ok

--- new/js/hero.js
+++ new/js/hero.js
@@ -4,14 +4,16 @@ const ctx = canvas.getContext("2d");
 class Logo {
   constructor() {
     this.img = new Image();
-    this.img.src = "./../img/kelbaz_logo.svg";
+    this.img.onload = () => {
+      this.img.src = "./img/kelbaz_logo.svg";
+    };
 
     this.x = canvas.width / 2;
     this.y = canvas.height / 2;
   }
 
   draw() {
-    this.img.onload = function () {
+    this.img.onload = () => {
       ctx.drawImage(this.img, 0, 0);
     };
   }
