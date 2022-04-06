0ae20958998149646d757b1c5a3255e43f241bad
Kelbaz <81097895+Kodbaz@users.noreply.github.com>
2022-04-06 17:50:57 +0200

    ok

--- new/js/index.js
+++ new/js/index.js
@@ -1,18 +1,20 @@
 if (!localStorage.getItem("jokeMade")) {
   localStorage.setItem("jokeMade", "yesSir !");
 
-  document.body.innerHTML += `
-  <div class="cookie-banner">
+  let cookie = document.createElement("div");
+  cookie.classList.add("cookie-banner");
+  cookie.innerHTML = `
     <div>
       Hey cookies ! La j'ai dis cookies et t'es en panic,<br />
       mais en vrai j'en ai rien a foutre de ces trucs !<br />
       Donc ce message sert juste a faire chier ! Merci !
     </div>
     <button class="cookie-btn">Ok ?</button>
-  </div>
-  `
+  `;
+
+  document.body.appendChild(cookie);
 
   document.querySelector(".cookie-btn").addEventListener("click", () => {
     document.querySelector(".cookie-banner").style.display = "none";
-  })
+  });
 }
