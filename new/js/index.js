f8bfce6941f6787eca287d53f57521ea34a7d0e6
Kelbaz <81097895+Kodbaz@users.noreply.github.com>
2022-04-06 17:43:09 +0200

    ok

--- new/js/index.js
+++ new/js/index.js
@@ -2,6 +2,17 @@ if (!localStorage.getItem("jokeMade")) {
   localStorage.setItem("jokeMade", "yesSir !");
 
   document.querySelector("body") += `
-  <div></div>
+  <div class="cookie-banner">
+    <div>
+      Hey cookies ! La j'ai dis cookies et t'es en panic,<br />
+      mais en vrai j'en ai rien a foutre de ces trucs !<br />
+      Donc ce message sert juste a faire chier ! Merci !
+    </div>
+    <button class="cookie-btn">Ok ?</button>
+  </div>
   `
+
+  document.querySelector(".cookie-btn").addEventListener("click", () => {
+    document.querySelector(".cookie-banner").style.display = "none";
+  })
 }
